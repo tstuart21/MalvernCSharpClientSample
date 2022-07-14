93b2264e21a0194218ae639363cf8926d75fb19e
Tom Stuart <tstuart@malvernsys.com>
2022-07-14 14:10:33 -0400

    Add project files.

---
+++ MalvernClientSample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Malvern;
+
+public class MalvernClientSample
+{
+
+    public static void Main(String[] args)
+    {
+        var testCases = GetTestCases();
+        foreach (var request in testCases)
+        {
+            Console.WriteLine($"Request:\r\n{request}");
+            Console.WriteLine("\r\nPress [Enter] to process transaction...\r\n");
+            Console.ReadLine();
+            
+            var reply = Malvern.Connection.SendTrans(request);
+            Console.WriteLine($"Response:\r\n{reply}");
+            Console.WriteLine("\r\nPress [Enter] to continue...\r\n");
+            Console.ReadLine();
+        }
+        Console.WriteLine("\r\nDone");
+        Console.ReadLine();
+    }
+
+    private static List<string> GetTestCases()
+    {
+        return new List<string>()
+        {
+            //Get Order (009)
+            "0,\"009\"1,\"126\"99,\"\"",
+            //Rate Single Method (002)
+            "0,\"002\"1,\"12345\"12,\"Contact\"11,\"Company\"13,\"40 Lloyd Ave\"14,\"Suite 103\"15,\"Malvern\"16,\"PA\"17,\"19355\"50,\"\"18,\"6101112233\"21,\"10\"19,\"UPS\"22,\"GND\"24,\"20211108\"6001,\"11082021112233\"99,\"\"",
+            //Ship Single Package (001)
+            "0,\"001\"1,\"12345\"12,\"Contact\"11,\"Company\"13,\"40 Lloyd Ave\"14,\"Suite 103\"15,\"Malvern\"16,\"PA\"17,\"19355\"50,\"\"18,\"6101112233\"21,\"10\"19,\"UPS\"22,\"GND\"24,\"20211108\"6001,\"11082021112233\"99,\"\""
+        };  
+
+    }
+
+
+
+   
+}
---
+++ MalvernComm/Connection.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malvern
+{
+    public class Connection
+    {
+        private static readonly int malvernPort = 1022;
+        private static readonly string malvernServer = "108.52.240.107";
+        
+        public static string SendTrans(string tcpRequest)
+        {
+            var bytes = new byte[1024];
+            var tcpResponse = "";
+            try
+            {
+                LogLine("Request: " + tcpRequest);
+                var sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    sender.Connect(malvernServer, malvernPort);
+                    LogLine("Socket connected to " + sender.RemoteEndPoint.ToString());
+                    int bytesRec;
+                    byte[] msg = Encoding.ASCII.GetBytes(tcpRequest);
+                    int bytesSent = sender.Send(msg);
+
+                    while (tcpResponse.ToString().IndexOf("99,\"\"") == -1)
+                    {
+                        bytesRec = sender.Receive(bytes);
+                        tcpResponse += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        System.Threading.Thread.Sleep(3);
+                    }
+
+                    LogLine("Response: " + tcpResponse);
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+                catch (ArgumentNullException ane)
+                {
+                    LogLine("ArgumentNullException: " + ane.Message);
+                    return "ArgumentNullException: " + ane.Message;
+                }
+                catch (SocketException se)
+                {
+                    LogLine("SocketException: " + se.Message);
+                    return "SocketException: " + se.Message;
+                }
+                catch (Exception e)
+                {
+                    LogLine("SocketException: " + e.Message);
+                    return "SocketException: " + e.Message;
+                }
+            }
+            catch (Exception e)
+            {
+                LogLine("Exception: " + e.Message);
+                return "Exception: " + e.Message;
+            }
+            return tcpResponse;
+        }
+        private static void LogLine(string message)
+        {
+            try
+            {
+                Directory.CreateDirectory("c:\\malvern");
+                Directory.CreateDirectory("c:\\malvern\\logs");
+                File.AppendAllText(Path.Combine("c:\\malvern\\logs",$"malvern_connection_{DateTime.Now.ToString("yyyyMMdd")}.txt"), DateTime.Now.ToString() + " " + message +"\r\n");
+            }
+            catch (Exception) { }
+        }
+    }
+}
